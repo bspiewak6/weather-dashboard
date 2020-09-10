a76c122e0a5b58ea376f72935396bc83c4d60790
Spiewak <bspiewak6@gmail.com>
2020-09-10 12:04:27 -0400

    pseudocode js file

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// pseudocode
+// user enters city into search input  (input id = searchTerm)
+// need to add button on click function (button id = searchBtn)
+
+// get the value of the city or input from user
+// clear input box
+// make a list of previously searched locations and append to the page (ul created in HTML)
+
+// use the full url to call the API
+// check the response from the API
+// console log responses
+
+// get the full forecast for city searched and append to the page (id = currentCity)
+// get the 5 day forecast for city searched and append to the page (id = fiveDayForecast)
