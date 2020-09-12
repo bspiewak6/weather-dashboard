a7e7b5131446ff3adf13e6deea3cc14e881553b5
Spiewak <bspiewak6@gmail.com>
2020-09-12 14:04:27 -0400

    updated js file with variables and function to fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,35 @@
 // pseudocode
-// global variables
+// global variables (city form, city, current weather, searched city, forecast, five-day-forecast, past search)
 var cities = [];
-var cityFormEl = document.querySelector("#city");
-var pastSearchButtonEl = document.querySelector("#past-search-buttons")
+var cityFormEl = document.querySelector("#city-search-form");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+var cityInputEl = document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 
-
-// create function to fetch weather api
+// create function 
+// then fetch weather api
 // use the full url to call the API and add inputValue
 // check the response from the API
 // console log response 
-
+var formSumbitHandler = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a city name");
+    }
+    saveSearch();
+    pastSearch(city);
+}
 
 var getCityWeather = function(city){
     var apiKey = "4487576f5b4f3e349130b486a36f052e"
@@ -20,13 +39,18 @@ var getCityWeather = function(city){
     .then(function(response){
         response.json().then(function(data){
             displayWeather(data, city);
-        });
-    });
+        })
+    })
 };
 
+var displayWeather = function(weather, searchCity){
+    //clear old content
+    weatherContainerEl.textContent= "";  
+    citySearchInputEl.textContent=searchCity;
+
+    console.log(weather);
 
 cityFormEl.addEventListener("submit", formSumbitHandler);
-pastSearchButtonEl.addEventListener("click", pastSearchHandler);
 
 // search button shows weather for city type in 
 
@@ -41,4 +65,4 @@ pastSearchButtonEl.addEventListener("click", pastSearchHandler);
 // full forecast for city searched and append to the page (id = currentCity)
 // 5 day forecast for city searched and append to the page (id = fiveDayForecast)
 
-  
+}
