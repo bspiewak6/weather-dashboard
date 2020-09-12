2f18d1084eb5118134f081f8c3d3392c871f58d8
Spiewak <bspiewak6@gmail.com>
2020-09-12 13:47:06 -0400

    re-formatted HTML file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,44 @@
 // pseudocode
 // global variables
-var button = document.querySelector("#searchBtn");
-var inputValue = document. querySelector("#searchText");
-var responseContainerEl = document.querySelector("#form-control");
+var cities = [];
+var cityFormEl = document.querySelector("#city");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons")
 
-// need to add event listener for search button
+
+
+// create function to fetch weather api
 // use the full url to call the API and add inputValue
 // check the response from the API
-// console log response data
-button.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + 
-    '&appid=4487576f5b4f3e349130b486a36f052e')
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(data) {
-            console.log(data);
-        })
-        // catch any errors
-        .catch(function(){
+// console log response 
+
+
+var getCityWeather = function(city){
+    var apiKey = "4487576f5b4f3e349130b486a36f052e"
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+            displayWeather(data, city);
         });
-    })
+    });
+};
 
 
-// clear input box after the user hits the search button
+cityFormEl.addEventListener("submit", formSumbitHandler);
+pastSearchButtonEl.addEventListener("click", pastSearchHandler);
 
+// search button shows weather for city type in 
 
-// get the full forecast for city searched and append to the page (id = currentCity)
-// get the 5 day forecast for city searched and append to the page (id = fiveDayForecast)
+// show city name
+// date
+// weather icon 
+// temp
+// humidity
+// wind speed
+// uv index -- need second API call
 
+// full forecast for city searched and append to the page (id = currentCity)
+// 5 day forecast for city searched and append to the page (id = fiveDayForecast)
 
-// make a list of previously searched locations and append to the page (ul created in HTML)
-
-
-// var nameValue = data['name'];
-//             var timeValue = data['timezone'];
-//             var tempValue = data['main']['temp'];
-//             var humValue = data['main']['humidity'];
-//             var windValue = data['wind']['speed'];
-//             // uv index?
-
-            // currentCity.innerHTML = nameValue;
-            // currentCity.innerHTML = timeValue;
-            // currentCity.innerHTML = tempValue;
-            // currentCity.innerHTML = humValue;
-            // currentCity.innerHTML = windValue;
+  
