dece5d1ed93e19006e6af50999336629edb6e3fb
Spiewak <bspiewak6@gmail.com>
2020-09-13 11:42:39 -0400

    updated five day forecast to show date, icon, temp and humidity

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,28 @@ var cityFormEl = document.querySelector("#city-search-form");
 var cityInputEl = document.querySelector("#city");
 var weatherContainerEl = document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
-var forecastTitle = document.querySelector("#forecast").style.padding = "20px";
+var forecastTitle = document.querySelector("#forecast");
 var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 var cities = [];
 
+var formSumbitButton = function(event) {
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        getFiveDay(city);
+        // clear out input value 
+        cityInputEl.value = "";
+    } else {
+        alert("Please enter a correct city");
+    }
+}
 // create function to get weather input from the search form
 // then fetch weather api
 // use the full url to call the API and add inputValue
 // check the response from the API
 // console log response 
-
 
 
 // create saveSearch function for localStorage
@@ -42,12 +53,14 @@ var displayWeather = function(weather, searchCity){
 
     // create an element for the date to go in next to the city that was searched by the user
     var currentDate = document.createElement("span")
-    currentDate.textContent=" (" + moment(weather.dt.value).format("L") + ") ";
+    currentDate.textContent=" (" + moment(weather.dt.value).format("l") + ") ";
+    // append date to weather container next to city name
     citySearchInputEl.appendChild(currentDate);
     
     // create an image element
     var weatherIcon = document.createElement("img")
     weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`);
+    // append icon to weather container next to date
     citySearchInputEl.appendChild(weatherIcon);
 
     // create a span element to hold temperature data
@@ -146,50 +159,57 @@ var displayFiveDay = function(weather) {
     var forecast = weather.list;
         for(i = 5; i < forecast.length; i = i + 8) {
             var weatherForecast = forecast[i];
-            console.log(weatherForecast);
-        
+            // console.log(weatherForecast);
 
         // create a card for each day
         var forecastEl = document.createElement("div");
-        forecastEl.classList = "card bg-primary text-light m-2";
+        forecastEl.classList = "card bg-info text-light m-4";
 
         
         // date
         var forecastDate = document.createElement("h5");
-        forecastDate.textContent = moment.unix(weatherForecast.dt.value).format("L");  
-        forecastDate.classList = "card-header text-center";
-        
+        forecastDate.classList = "card-header text-left";
+        forecastDate.textContent = moment.unix(weatherForecast.dt).format("l"); 
         // append date
         forecastEl.appendChild(forecastDate); 
         
-        
         // weather condition icon
+        var weatherIcon = document.createElement("img");
+        weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${weatherForecast.weather[0].icon}.png`);
+        // append icon
+        forecastEl.appendChild(weatherIcon);
+        
         // temp
+        var forecastTemp = document.createElement("span");
+        forecastTemp.classList = "card-body text-left";
+        forecastTemp.innerHTML = "Temp: " + (Math.round(weatherForecast.main.temp * 10) / 10) + " &#176F";
+        // append temp
+        forecastEl.appendChild(forecastTemp);
+
         // humidity
-        }
+        var forecastHum = document.createElement("span");
+        forecastHum.classList = "card-body text-left";
+        forecastHum.innerHTML = "Humidity: " + weatherForecast.main.humidity + " %";
+        // append humidity
+        forecastEl.appendChild(forecastHum);
+
+        // console.log(forecastEl);
+        // append all of forecast date, icon, temp and humidity to the forecast five day container
+        forecastContainerEl.appendChild(forecastEl);
+        
+    }
 };
 
 
 
 
-var formSumbitButton = function(event) {
-    event.preventDefault();
-    var city = cityInputEl.value.trim();
-    if(city){
-        getCityWeather(city);
-        getFiveDay(city);
-        // clear out input value 
-        cityInputEl.value = "";
-    } else {
-        alert("Please type in correct city");
-    }
-}
+
 
 // save city name in localStorage and add pastSeach function to add city names to searched list
-var pastSeach = function(pastSeach) {
-    console.log(pastSearch);
+// var pastSeach = function(pastSeach) {
+//     console.log(pastSearch);
    
-}
+// }
 
 cityFormEl.addEventListener("submit", formSumbitButton);
 // need to create event listener for pastSearch buttons
